f05cf67861b2c17f8fb8b4e37019c2748fd59d7b
oleksandr <oleksandr.mamchii@gmail.com>
2023-02-16 17:49:55 +0200

    services: refactored pom

--- docker-demo2/src/main/java/com/docker/dockerdemo2/repository/LanguageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docker.dockerdemo2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docker.dockerdemo2.model.Language;
-
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-  
-}
